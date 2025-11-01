4dc04571ed00da0f833c4eb08fda078e37bc77c5
Phuong <np384874@gmail.com>
2025-11-01 20:33:54 +0700

    Thêm cho có, chứ không thật sự cần thiết (đã comment)

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Do đã khai báo bên index.js rồi nên không cần khai báo ở đây
+// var aboutRouter = require('./routes/about');
+// var contactRouter = require('./routes/contact');
+// var carRentRouter = require('./routes/car_rent');
+// var hotelsRouter = require('./routes/hotel');
+// var placesRouter = require('./routes/places');
+// var signin_signoutRouter = require('./routes/signin_signup');
+
 var app = express();
 
 // view engine setup
@@ -22,6 +30,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// Do đã khai báo bên index.js rồi nên không cần khai báo ở đây
+// app.use('/about', aboutRouter);
+// app.use('/contact', contactRouter);
+// app.use('/car_rent', carRentRouter);
+// app.use('/hotel', hotelsRouter);
+// app.use('/places', placesRouter);
+// app.use('/signin_signup', signin_signoutRouter);
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -35,7 +52,9 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+    res.render('error', {
+        layout: false,  // Tắt layout
+    });
 });
 
 module.exports = app;
