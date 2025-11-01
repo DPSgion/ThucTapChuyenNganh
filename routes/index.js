7079043ffbfbd3e5c61e0992200da854c006f127
DPSgion <np384874@gmail.com>
2025-11-01 19:32:21 +0700

    Làm gọn lại các file hbs, những cái thành phần trùng (header, footer, ...) thì bỏ qua bên layout.hbs

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('index', { title: 'Du lịch miền Nam'});
 });
+
 router.get('/about', function(req, res, next) {
-    res.render('about');
+    res.render('about', { title: 'Du lịch miền Nam'});
 });
 router.get('/places', function(req, res, next) {
-    res.render('places');
+    res.render('places', { title: 'Du lịch miền Nam'});
 });
 router.get('/hotel', function(req, res, next) {
-    res.render('hotel');
+    res.render('hotel', { title: 'Du lịch miền Nam'});
 });
 router.get('/blog', function(req, res, next) {
-    res.render('blog');
+    res.render('blog', { title: 'Du lịch miền Nam'});
 });
 router.get('/contact', function(req, res, next) {
-    res.render('contact');
+    res.render('contact', { title: 'Du lịch miền Nam'});
 });
 
 module.exports = router;
